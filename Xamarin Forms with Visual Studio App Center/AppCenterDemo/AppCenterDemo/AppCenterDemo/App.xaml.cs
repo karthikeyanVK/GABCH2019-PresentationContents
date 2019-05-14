bee95a08ac2b137ba960192b5b0f9bb838f42ddb
Anbu Mani <anbumani91@hotmail.com>
2019-05-15 00:16:44 +0530

    Xamarin Forms with Visual Studio App Center

---
+++ Xamarin Forms with Visual Studio App Center/AppCenterDemo/AppCenterDemo/AppCenterDemo/App.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace AppCenterDemo
+{
+    public partial class App : Application
+    {
+        public string LogTag { get; private set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+
+
+            Crashes.SetEnabledAsync(true);
+            Microsoft.AppCenter.AppCenter.Start("ios={Your iOS App secret here};" +
+               "uwp={Your UWP App secret here};" +
+               "android=1365703f-f659-48fb-acfe-38e00b07e0a3;",
+               typeof(Analytics), typeof(Crashes));
+
+            Crashes.ShouldAwaitUserConfirmation = ConfirmationHandler;
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+        private bool ConfirmationHandler()
+        {
+            Xamarin.Forms.Device.BeginInvokeOnMainThread(() =>
+            {
+                Current.MainPage.DisplayActionSheet("Crash detected. Send anonymous crash report?", null, null, "Send", "Always Send", "Don't Send").ContinueWith((arg) =>
+                {
+                    var answer = arg.Result;
+                    UserConfirmation userConfirmationSelection;
+                    if (answer == "Send")
+                    {
+                        userConfirmationSelection = UserConfirmation.Send;
+                    }
+                    else if (answer == "Always Send")
+                    {
+                        userConfirmationSelection = UserConfirmation.AlwaysSend;
+                    }
+                    else
+                    {
+                        userConfirmationSelection = UserConfirmation.DontSend;
+                    }
+                    Microsoft.AppCenter.AppCenterLog.Debug(LogTag, "User selected confirmation option: \"" + answer + "\"");
+                    Crashes.NotifyUserConfirmation(userConfirmationSelection);
+                });
+            });
+
+            return true;
+        }
+    }
+}
---
+++ Xamarin Forms with Visual Studio App Center/AppCenterDemo/AppCenterDemo/AppCenterDemo/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCenterDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        string error;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEvent_Clicked(object sender, EventArgs e)
+        {
+            Analytics.TrackEvent("Clicked Event Button");
+        }
+
+        private void BtnCrash_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                //Crashes.GenerateTestCrash();  --First Test Crash 
+                error = "xyz"; 
+                var crash = Convert.ToInt32(error);
+                
+            }
+            catch (Exception ex)
+            {
+                Crashes.TrackError(ex);
+            }
+        }
+    }
+}
